b88db429d04dc67b1b92a80a37c6acdb862ccacb
Ali Akbar <ali.zahra.nice@gmail.com>
2018-08-23 09:24:15 +0430

    chapter 3-string and things

--- src/chapter3_Strings_and_things/TestImmutableString.java
+++ src/chapter3_Strings_and_things/TestImmutableString.java
@@ -2,11 +2,13 @@ package chapter3_Strings_and_things;
 
 /**
  * <a href="https://www.javatpoint.com/immutable-string" />
+ * <a href="https://stackoverflow.com/questions/8798403/string-is-immutable-what-exactly-is-the-meaning" />
  */
 public class TestImmutableString {
 
     public static void main(String args[]) {
         String s = "Sachin";
+        String str = s;     // assigns a new reference to the same string "knowledge"
         s.concat(" Tendulkar");//concat() method appends the string at the end
         System.out.println(s);//will print Sachin because strings are immutable objects
     }
