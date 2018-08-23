92f40125d3887cea8c3badd1130f604c10eff3d3
Ali Akbar <ali.zahra.nice@gmail.com>
2018-08-23 19:02:40 +0430

    chapter 3-taking strings apart

---
+++ src/chapter3_Strings_and_things/SubStringDemo.java
@@ -0,0 +1,18 @@
+package chapter3_Strings_and_things;
+
+public class SubStringDemo {
+
+    public static void main(String[] args) {
+        String a = "Java is great.";
+        System.out.println(a);
+        String b = a.substring(5);
+        // b is the String "is great."
+        System.out.println(b);
+        String c = a.substring(5,7);// c is the String "is"
+        System.out.println(c);
+        String d = a.substring(5,a.length());// d is "is great."
+        System.out.println(d);
+
+    }
+
+}
